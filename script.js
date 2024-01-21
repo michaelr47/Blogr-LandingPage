f56e3150384a47b2a65a64e2042bb9fd305b842f
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-01-20 20:48:52 -0800

    add ternary to toggle displaying the popup menu, also nav items

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburgerIcon = document.querySelector('.hamburgerIcon');
 const loginLink = document.querySelector('.loginLink');
 const signUpBtn  = document.querySelector('.signUpButton');
 // let navList = document.querySelectorAll('li:not(:first-child)');
+const mobileNavItems = Array.from(document.querySelectorAll('.navPopupMenu ul li'));
 
 const windowWidth = () => { 
     let width = window.innerWidth;
@@ -14,22 +15,23 @@ const windowWidth = () => {
       hamburgerIcon.style.display = 'none';
       loginLink.classList.remove('hidden');
       signUpBtn.classList.remove('hidden');
-    }
-    
-    
-  }
+    }  
+}
   
 hamburgerIcon.addEventListener('click', () => {
-  let navPopupMenu = document.querySelectorAll('.navPopupMenu');
-  navPopupMenu.style.display = 'inline';
+  let navPopupMenu = document.querySelector('.navPopupMenu');
+  navPopupMenu.style.display = (navPopupMenu.style.display === 'none') ? 'block' : 'none';
 });
-
-const mobileNavItems = Array.from(document.querySelectorAll('.navPopupMenu ul li'));
 
 const toggleMobileNavItem = item => {
   item.addEventListener('click', () => {
-    let itemContents = item.querySelector('sub-nav-items');
-  })
+    let itemContents = item.querySelector('.sub-nav-items');
+    itemContents.style.display = (itemContents.style.display === 'none') ? 'block' : 'none';
+  });
 };
+
+for (const item of mobileNavItems) {
+  toggleMobileNavItem(item);
+}
 window.addEventListener('resize', windowWidth);
 document.addEventListener('DOMContentLoaded', windowWidth);
